5589c04e3d78a9899bac753da98fbfe201eb888f
Thomas Yau <yaucp@connect.hku.hk>
2023-11-27 19:32:36 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yau Chin Pang on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
